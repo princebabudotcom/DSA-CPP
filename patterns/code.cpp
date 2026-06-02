eeb81784c7ae4054e2449e47862e45c6483ccf48
Prince Babu <princebabu4495@gmail.com>
2026-06-02 21:40:46 +0530

    DAY 2 DSA : Practice more DSA pattern questions

--- patterns/code.cpp
+++ patterns/code.cpp
@@ -68,7 +68,7 @@ int main()
     return 0;
 }
 
-/**
+/*
  * Traingle pattern
  */
 
@@ -103,6 +103,136 @@ int main()
         for (int j = i + 1; j > 0; j--)
         {
             cout << j << " ";
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
+
+/*
+ * Floyd's Triangle pattern
+ * with numbers   n = 4
+ *
+ */
+
+int main()
+{
+
+    int num = 1, n;
+
+    cout << "Enter a number : " << " ";
+    cin >> n;
+
+    /*
+     * 1
+     * 2 3
+     * 4 5 6
+     * 7 8 9 10
+     */
+
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = i + 1; j > 0; j--)
+        {
+            cout << num << " ";
+            num++;
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
+
+/*
+ * Inverted pattern
+ */
+
+int main()
+{
+    int num = 1, n;
+
+    cout << "Enter a number : " << " ";
+    cin >> n;
+
+    for (int i = 0; i < n; i++)
+    {
+        // spaces
+        for (int j = 0; j < i; j++)
+        {
+            cout << " ";
+        }
+
+        // nums
+
+        for (int j = 0; j < (n - i); j++)
+        {
+            cout << (i + 1);
+        }
+
+        cout << endl;
+    }
+
+    return 0;
+}
+
+/*
+ * Pyramid pattern
+ */
+
+int main()
+{
+    int num = 1, n;
+
+    cout << "Enter a number : " << " ";
+    cin >> n;
+
+    /*
+     *          1
+     *         1 2 1
+     *       1 2 3 1 2
+     *     1 2 3 4 1 2 3
+     */
+
+    for (int i = 0; i < n; i++)
+    {
+        // for spaces
+
+        for (int j = 0; j < (n - i - 1); j++)
+        {
+            cout << " ";
+        }
+
+        // for traingle 1
+        for (int j = 0; j < i + 1; j++)
+        {
+            cout << j + 1;
+        }
+
+        for (int j = i; j > 0; j--)
+        {
+            cout << j;
+        }
+
+        cout << endl;
+    }
+
+    return 0;
+}
+
+int main()
+{
+
+    int n;
+    cout << "Enter a number :" << " ";
+    cin >> n;
+
+    for (int i = 0; i < n; i++)
+    {
+        // this is not run at 0
+        for (int j = i + 1; j > 0; j--)
+        {
+            cout << j;
         }
         cout << endl;
     }
